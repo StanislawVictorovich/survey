9e01e13070200947f240b69ca03580378a5105cd
Stanislaw <svirepov.stanislav@gmail.com>
2019-03-13 11:29:22 +0200

    survey component

--- src/components/Survey/Survey.vue
+++ src/components/Survey/Survey.vue
@@ -1,11 +1,56 @@
 <template>
-  <h1>Survey works!!!</h1>  
+  <md-steppers :md-active-step.sync="active" md-vertical md-linear>
+    <md-step 
+      id="first" 
+      md-label="First Step" 
+      md-description="Optional" 
+      :md-editable="false" 
+      :md-done.sync="first">
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias doloribus eveniet quaerat modi cumque quos sed, temporibus nemo eius amet aliquid, illo minus blanditiis tempore, dolores voluptas dolore placeat nulla.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias doloribus eveniet quaerat modi cumque quos sed, temporibus nemo eius amet aliquid, illo minus blanditiis tempore, dolores voluptas dolore placeat nulla.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias doloribus eveniet quaerat modi cumque quos sed, temporibus nemo eius amet aliquid, illo minus blanditiis tempore, dolores voluptas dolore placeat nulla.</p>
+        <md-button class="md-raised md-primary" @click="setDone('first', 'second')">Continue</md-button>
+    </md-step>
+
+    <md-step id="second" md-label="Second Step" :md-error="secondStepError" :md-editable="false" :md-done.sync="second">
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias doloribus eveniet quaerat modi cumque quos sed, temporibus nemo eius amet aliquid, illo minus blanditiis tempore, dolores voluptas dolore placeat nulla.</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias doloribus eveniet quaerat modi cumque quos sed, temporibus nemo eius amet aliquid, illo minus blanditiis tempore, dolores voluptas dolore placeat nulla.</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias doloribus eveniet quaerat modi cumque quos sed, temporibus nemo eius amet aliquid, illo minus blanditiis tempore, dolores voluptas dolore placeat nulla.</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias doloribus eveniet quaerat modi cumque quos sed, temporibus nemo eius amet aliquid, illo minus blanditiis tempore, dolores voluptas dolore placeat nulla.</p>
+      <md-button class="md-raised md-primary" @click="setDone('second', 'third')">Continue</md-button>
+      <md-button class="md-raised md-primary" @click="setError()">Set error!</md-button>
+    </md-step>
+
+  </md-steppers>
 </template>
 
 <script>
-export default {
-  
-}
+  export default {
+    name: 'StepperNonEditable',
+    data(){
+      return {
+        active: 'first',
+        first: false,
+        second: false,
+        third: false,
+        secondStepError: null
+      }
+    },
+    methods: {
+      setDone (id, index) {
+        this[id] = true
+
+        this.secondStepError = null
+
+        if (index) {
+          this.active = index
+        }
+      },
+      setError () {
+        this.secondStepError = 'This is an error!'
+      }
+    }
+  }
 </script>
 
 <style lang="scss" scoped>
