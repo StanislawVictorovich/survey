a1a27d7fa91cc3a5ca364d0922e5740329a9b6dd
Stanislaw <svirepov.stanislav@gmail.com>
2019-03-13 11:11:20 +0200

    add registration component

--- src/components/Registration/Registration.vue
+++ src/components/Registration/Registration.vue
@@ -1,13 +1,64 @@
 <template>
-  <h1>Registration works!</h1>
+  <div class="registration">
+
+    <md-field :class="messageClass">
+      <label>First name</label>
+      <md-input v-model="firstName" required @keydown="hasMessages = false"></md-input>
+      <span class="md-error">First name is required</span>
+    </md-field>
+
+    <md-field>
+      <label>Last name</label>
+      <md-input v-model="lastName" ></md-input>
+    </md-field>
+
+    <md-datepicker v-model="date">
+      <label>Select your birth date</label>
+    </md-datepicker>
+
+    <md-button class="md-primary md-raised" @click="submit">Start</md-button>
+
+  </div>
 </template>
 
 <script>
-export default {
-  
-}
+import { mapActions } from 'vuex'
+
+  export default {
+    name: 'ErrorsMessages',
+    data(){
+      return {
+        firstName: null,
+        lastName: null,
+        date: null,
+        hasMessages: false
+      }
+    },
+    computed: {
+      messageClass () {
+        return {
+          'md-invalid': this.hasMessages
+        }
+      }
+    },
+    methods: {
+      ...mapActions(['saveFirstName', 'saveLastName', 'saveDate']),
+      submit() {
+        if (this.firstName) {
+          this.saveFirstName(this.firstName);
+          this.saveLastName(this.lastName);
+          this.saveDate(this.date);
+          this.$router.push('survey');
+        } else {
+          this.hasMessages = true;
+        }
+      }
+    }
+  }
 </script>
 
 <style lang="scss" scoped>
-
+  .registration {
+    margin: 2em;
+  }
 </style>
