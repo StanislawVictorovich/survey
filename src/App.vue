0dd72ea07e7344044bddd7291d41f861bfd30e6e
Stanislaw <svirepov.stanislav@gmail.com>
2019-03-13 07:44:32 +0200

    started project

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,9 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+<!--      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>-->
+      <md-button class="md-raised md-primary">Start</md-button>
     </div>
     <router-view/>
   </div>
