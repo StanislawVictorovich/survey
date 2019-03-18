e7bdacd95f49e76c9b2c96233f81bf45d4be201b
Stanislaw <svirepov.stanislav@gmail.com>
2019-03-18 20:24:42 +0200

    add another store mechanism

--- src/services/Storage.js
+++ src/services/Storage.js
@@ -7,5 +7,18 @@ export default {
   },
   clear() {
     localStorage.clear();
+  },
+  storage_id: 'user_data',
+  setUserData(user) {
+    const storedUser = this.getUserData();
+    for (let key in storedUser) {
+      if (!user[key]) {
+        user[key] = storedUser[key];
+      }
+    }
+    localStorage.setItem(this.storage_id, JSON.stringify(user));
+  },
+  getUserData() {
+    return JSON.parse(localStorage.getItem(this.storage_id));
   }
 }
