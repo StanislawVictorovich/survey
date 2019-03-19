b76117cb57b845f977403f53649611d93c55ddca
Stanislaw <svirepov.stanislav@gmail.com>
2019-03-19 08:32:19 +0200

    translate questions to eng

--- src/store/index.js
+++ src/store/index.js
@@ -11,41 +11,56 @@ export default new Vuex.Store({
       testCompleted: false,
       questions: [
         {
-          title: 'Чему равна длина arr.length массива arr?',
-          choises: [
-            '0', '1', '2', '3', '4', 'больше'
-          ],
-          correct: 3
-        },
-        {
-          title: `Что выведет этот код? <br>
+          title: `What will this code outputs? <br>
           <code>alert( 20e-1['toString'](2) );</code>`,
           choises: [
-            '2', '10', '20', 'NaN', '4', 'в коде ошибка'
+            '2', '10', '20', 'NaN', '4', 'there is an error'
           ],
           correct: 3
         },
         {
-          title: `Что делает код:<br><code>break me;</code>`,
+          title: `What does this code do:<br><code>break me;</code>`,
           choises: [
-            'Ломает интерпретатор javascript.', 'Выходит из текущего блока цикла или switch на метку «me».', 'Выдает ошибку.'
+            'breakes javascript.', 'exits loop on tag «me».', 'generetes error.'
           ],
           correct: 1
         },
         {
-          title: 'Чему равна сумма [] + 1 + 2?',
+          title: 'What os the sum of [] + 1 + 2?',
           choises: [
-            '1', 'NaN', 'undefined', '12', 'другое'
+            '1', 'NaN', 'undefined', '12', 'other'
           ],
           correct: 4
         },
         {
-          title: `Какой результат будет у выражения ниже?<br><code>null + {0:1}[0] + [,[1],][1][0]</code>`,
+          title: `What the result will be of the expression below?<br><code>null + {0:1}[0] + [,[1],][1][0]</code>`,
           choises: [
             '0', '1', '2', 'undefined', 'NaN'
           ],
           correct: 2
-        }
+        },
+        {
+          title: `What does this code do:<br><code>let a = (1,5 - 1) * 2;<br>alert(a);</code>`,
+          choises: [
+            '0.9999999999999', '0.5', '8', '-0.5', '4'
+          ],
+          correct: 2
+        },
+        {
+          title: `What the result will be of the expression below?<br><code>alert( 20e-1['toString'](2) );</code>`,
+          choises: [
+            '2', '10', '20', 'NaN', 'there is an error in code'
+          ],
+          correct: 1
+        },
+        {
+          title: `What does this code do:<br><code>function User() { }<br>User.prototype = { admin: false };<br><br>
+          let user = new User();<br><br>User.prototype = { admin: true };<br><br>alert(user.admin); </code>`,
+          choises: [
+            'false', 'undefined', 'true'
+          ],
+          correct: 0
+        },
       ]
     }
   },
