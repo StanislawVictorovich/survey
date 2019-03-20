34d731e733ff2d1ba3ffaaadceb7859792268a65
Stanislaw <svirepov.stanislav@gmail.com>
2019-03-20 08:53:50 +0200

    add empty rows

--- src/services/Storage.js
+++ src/services/Storage.js
@@ -2,11 +2,15 @@ export default {
   storage_id: 'user_data',
   setUserData(user) {
     const storedUser = this.getUserData();
+
     for (let key in storedUser) {
+      
       if (!user[key]) {
         user[key] = storedUser[key];
       }
+      
     }
+
     localStorage.setItem(this.storage_id, JSON.stringify(user));
   },
   getUserData() {
