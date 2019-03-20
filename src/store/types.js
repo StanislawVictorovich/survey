32b1cdb2933429d6a7797e1e66b7ac55d53f6f1d
Stanislaw <svirepov.stanislav@gmail.com>
2019-03-20 08:54:44 +0200

    move constant to another type-file

--- src/store/types.js
+++ src/store/types.js
@@ -1,5 +1,6 @@
 export default {
   INCREMENT_CORRECT_ANSWER: 'INCREMENT_CORRECT_ANSWER',
   COMPLETE_TEST: 'COMPLETE_TEST',
-  PRODUCTION_MODE: 'production'
+  PRODUCTION_MODE: 'production',
+  questions: 'questions'
 }
