4ef496d0350313b84276e2e8dbbc4b4285a3841b
Stanislaw <svirepov.stanislav@gmail.com>
2019-03-21 07:30:53 +0200

    refactor

--- src/types/constants.js
+++ src/types/constants.js
@@ -1,8 +1,5 @@
-import Home from '../views/Home.vue';
-
 export default {
   regEmail: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-  questions: 'questions',
   SURV_ID: 'surv',
   START_INDEX_ELEMENT: 0
 }
