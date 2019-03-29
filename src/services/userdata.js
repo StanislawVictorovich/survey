0c4adfc32d8114fabc01e19f32c889bc3a0e5392
Stanislaw <svirepov.stanislav@gmail.com>
2019-03-30 00:41:08 +0200

    refactor & optimization add getters / setters generator factory

--- src/services/userdata.js
+++ src/services/userdata.js
@@ -4,87 +4,24 @@ import types from "./types";
 class UserData extends SecureStorage {
   constructor(key) {
     super(key);
-  }
-  get email() {
-    return this.getUserData().email;
-  }
-  set email(email) {
-    this.setUserData({ email });
-  }
-  get firstName() {
-    return this.getUserData().firstName;
-  }
-  set firstName(firstName) {
-    this.setUserData({ firstName });
-  }
-  get lastName() {
-    return this.getUserData().lastName;
-  }
-  set lastName(lastName) {
-    this.setUserData({ lastName });
-  }
-  get date() {
-    return this.getUserData().date;
-  }
-  set date(date) {
-    this.setUserData({ date });
-  }
-  get users() {
-    return this.getUserData().users;
-  }
-  set users(users) {
-    this.setUserData({ users });
-  }
-  get currentStep() {
-    return this.getUserData().currentStep;
-  }
-  set currentStep(currentStep) {
-    this.setUserData({ currentStep });
-  }
-  get answersMatrix() {
-    return this.getUserData().answersMatrix;
-  }
-  set answersMatrix(answersMatrix) {
-    this.setUserData({ answersMatrix });
-  }
-  get questions() {
-    return this.getUserData().questions;
-  }
-  set questions(questions) {
-    this.setUserData({ questions });
-  }
-  get testComplete() {
-    return this.getUserData().testComplete;
-  }
-  set testComplete(testComplete) {
-    this.setUserData({ testComplete });
+    types.STORAGE_FIELDS.forEach(item => { // getters-setters genereator factory
+      Object.defineProperty(this, item, {
+        get() {
+          return this.getUserData()[item];
+        },
+        set(value) {
+          this.setUserData(new function() { 
+            this[item] = value;
+          })
+        }
+      });
+    });
   }
   emailRegistered(email) {
     return !!this.getUser(email);
   }
   getUser(email) {
-    const { users } = this.getUserData();
-    if (users) {
-      return this.getUserData().users.filter(user => user.email === email)[0];
-    } else {
-      return false;
-    }
-  }
-  getUserAnswer(email, questionId) {
-    const user = this.getUser(email);
-    if (user) {
-      return user.answersMatrix[user.questionsIdMatrix.indexOf(questionId)];
-    } else {
-      return false;
-    }
-  }
-  saveNewUser() {
-    const { firstName, lastName, email, date, users } = this.getUserData();
-    if (this.emailRegistered(email) || !users) {
-      return false;
-    }
-    users.push({ firstName, lastName, email, date }); 
-    this.setUserData({ users });
+    return this.getUserData() ? this.getUserData().users.filter(user => user.email === email)[0] : false;
   }
 }
 
