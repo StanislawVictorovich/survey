85b72c1df142e97dfedba91db2da440b71f447f2
Stanislaw <svirepov.stanislav@gmail.com>
2019-03-30 00:40:21 +0200

    add array of getters / setters generator factory in class constructor

--- src/services/types.js
+++ src/services/types.js
@@ -1,3 +1,14 @@
 export default {
-  SECURE_KEY: 'user_data'
+  SECURE_KEY: 'user_data',
+  STORAGE_FIELDS: [
+    'firstName', 
+    'lastName', 
+    'date', 
+    'email', 
+    'users',
+    'currentStep',
+    'answersMatrix',
+    'questions',
+    'testComplete'
+  ]
 }
