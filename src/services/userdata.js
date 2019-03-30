db3d38dc001870d66714ffc172dae601be99c2bc
Stanislav <33198664+StanislawVictorovich@users.noreply.github.com>
2019-03-30 12:42:06 +0200

    rename constants

--- src/services/userdata.js
+++ src/services/userdata.js
@@ -1,10 +1,10 @@
 import { SecureStorage } from "./storage";
-import types from "./types";
+import constants from "./constants";
 
 class UserData extends SecureStorage {
   constructor(key) {
     super(key);
-    types.STORAGE_FIELDS.forEach(item => { // getters-setters genereator factory
+    constants.STORAGE_FIELDS.forEach(item => { // getters-setters genereator factory
       Object.defineProperty(this, item, {
         get() {
           return this.getUserData()[item];
@@ -25,4 +25,4 @@ class UserData extends SecureStorage {
   }
 }
 
-export default new UserData(types.SECURE_KEY);
+export default new UserData(constants.SECURE_KEY);
